e5a638bc510386c204b90121771e65ba86a984ff
kanjamad bosel <kanjamadpor@gmail.com>
2019-05-30 16:45:39 -0700

    Create CRUD for order and category

---
+++ controllers/orders.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+router.get('/', (req, res) => {
+    db.Order.find({}, (err, allOrders) => {
+        if (err) return res.status(500).json({status: 500, error: 'Something went wrong!!! Please try again'});
+        res.json({status: 200, message: 'Success', data: allOrders});
+    });
+});
+
+router.get('/:id', (req, res) => {
+    db.Order.findById(req.params.id, (err, foundOrder) => {
+        if (err) return res.status(500).json({status: 500, error: 'Something went wrong!!! Please try again'});
+        res.json({status: 200, message: 'Success', data: foundOrder});
+    });
+});
+
+router.post('/', (req, res) => {
+    db.Order.create(req.body, (err, newOrder) => {
+        if (err) return res.status(500).json({status: 500, error: 'Something went wrong!!! Please try again'});
+        res.json({status: 200, message: 'Success', data: newOrder});
+    });
+});
+
+router.put('/:id', (req, res) => {
+    db.Order.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedOrder) => {
+        if (err) return res.status(500).json({status: 500, error: 'Something went wrong!!! Please try again'});
+        res.json({status: 200, message: 'Success', data: updatedOrder});
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    db.Order.findByIdAndRemove(req.params.id, (err, deletedOrder) => {
+        if (err) return res.status(500).json({status: 500, error: 'Something went wrong!!! Please try again'});
+        res.json({status: 200, message: 'Success', data: deletedOrder});
+    });
+});
+
+
+module.exports = router;
