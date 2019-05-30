ecdd99faa437a6099871115b128c91c3177201da
kanjamad bosel <kanjamadpor@gmail.com>
2019-05-30 13:52:17 -0700

    create Auth signUp

---
+++ README.md
@@ -0,0 +1,14 @@
+
+<!-- ![Logo](assets/City_logo.png "Logo") -->
+
+
+### Express file tree
+
+With backend code to organize.
+![Logo](assets/tree.png "expree file tree")
+
+## Additional Resources
+1. <a href="http://expressjs.com/starter/installing.html" target="_blank">Starting an Express Project</a>
+2. <a href="http://expressjs.com/starter/hello-world.html" target="_blank">Express Hello World</a>
+3. <a href="http://expressjs.com/starter/static-files.html" target="_blank">Express Static Files</a>
+4. <a href="http://expressjs.com/4x/api.html#res.render" target="_blank">Express res.render()</a>
--- controllers/auth.js
+++ controllers/auth.js
@@ -38,6 +38,44 @@ router.post('/signup', (req, res) => {
         if (err) return res.json({user: req.body, errors: [{message: 'Something went wrong!!! Please try again'}]});
 
         // If we didn't find a user, re-json the login page with error message
-        
+        if (foundUser){
+            errors.push({message: 'Your passwords do not match'});
+        }
+    });
+
+    // If there are any validation errors, Re-json signup page with error messages
+    if (errors.length){
+        return res.json({user: req.body, errors: errors});
+    }
+
+    // Generate salt for additional password hash complexity
+    bcrypt.genSalt(10, (err, salt)=> {
+        if (err) return res.json({user: req.body, errors: [{message: 'Something went wrong!!! Please try again'}]});
+
+    // Hash user password from signup form
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) return res.json({user: req.body, errors: [{message: 'Something went wrong!!! Please try again'}]});
+
+        // Create an object to hold the new user information (with hashed password, not original password)
+        const userData = {
+            fullName: req.body.fullName,
+            email: req.body.email,
+            gender: req.body.gender,
+            password: hash,
+        }
+
+        // Create a new User record in MongoDB from the newUser object above
+        db.User.create(userData, (err, newUser) => {
+            if (err) return res.json({ errors: [err]});
+            
+            // If new user was created successfully, redirect user to login page
+            // We could also create the session here (just like the login route), then redirect to the dashboard instead
+            // res.redirect('/login');
+
+            res.json({status: 200, message: 'Success', newUser})
+        });
+    });
     });
 });
+
+// POST Login Route
